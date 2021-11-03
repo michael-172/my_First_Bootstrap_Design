aef370f36f9e31b3a19503b8b250e003deecf3a6
michael-172 <85269483+michael-172@users.noreply.github.com>
2021-11-04 00:59:44 +0200

    Delete Bootstrap 4 Template directory

--- Bootstrap 4 Template/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    'use strict';
-
-    var winH = $(window).height(),
-        uppH = $('.upperbar').innerHeight(),
-        navH = $('.navbar').innerHeight();
-    $('.slider, .carousel-item').height(winH - (uppH + navH));
-});
